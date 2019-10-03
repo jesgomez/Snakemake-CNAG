155706668291fd927ccc0829af2efe76f4ec8dfa
jesgomez <jessica.gomez.garrido@gmail.com>
2019-10-03 18:27:14 +0200

    sbatch log fix

--- sbatch-cnag.py
+++ sbatch-cnag.py
@@ -37,11 +37,10 @@ if "qos" in job_properties['cluster']:
 log = job_properties.get("log","slurm-%j.out" )
 
 if "log" in job_properties['cluster']:
-	log = job.properties['cluster']["log"][0]
-else:
-	log= "slurm-%j.out"
+	log = job.properties['cluster']["log"]
 
-cmdline.append("--parsable --job-name={name} --error={log} --output={log} --time={time} --partition={queue} --qos={qos} --cpus-per-task={cpus}".format(time=time, cpus=cpus,name=name,queue=queue, qos=qos, log=log))
+
+cmdline.append("--parsable --job-name={name} --error={log} --output={log} --time={time} --partition={queue} --qos={qos} --cpus-per-task={cpus}".format(time=time, cpus=cpus,name=name,queue=queue, qos=qos, log=log[0]))
 cmdline.append(jobscript)
 
 # Constructs and submits
